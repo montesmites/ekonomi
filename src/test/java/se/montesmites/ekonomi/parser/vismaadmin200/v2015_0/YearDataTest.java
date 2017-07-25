aa33b633bc13490d00bce79d3411255905a1ca4c
nrknik <nkrnik@montesmites.se>
2017-07-25 17:12:38 +0200

    extended tests for Year

--- src/test/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/YearDataTest.java
+++ src/test/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/YearDataTest.java
@@ -5,13 +5,17 @@ import java.io.InputStream;
 import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import se.montesmites.ekonomi.model.Year;
+import se.montesmites.ekonomi.model.YearId;
 import se.montesmites.ekonomi.parser.vismaadmin200.Parser;
 
 public class YearDataTest {
@@ -30,16 +34,28 @@ public class YearDataTest {
         Files.copy(source, target.toPath(),
                 new CopyOption[]{StandardCopyOption.REPLACE_EXISTING});
         Parser p = new Parser(tempfolder.getRoot().toPath());
-        List<Year> years = p.parse(BinaryFile_2015_0.YEARS);
-        assertEquals(4, years.size());
-    }
-
-    @Test
-    public void test() {
-        assertTrue(true);
+        Set<Year> exp = set(
+                year("C", "2012", "2012-01-01", "2012-12-31"),
+                year("D", "2013", "2013-01-01", "2013-12-31"),
+                year("E", "2014", "2014-01-01", "2014-12-31"),
+                year("F", "2015", "2015-01-01", "2015-12-31"));
+        Set<Year> act = set(p.parse(BinaryFile_2015_0.YEARS));
+        assertEquals(exp, act);
     }
 
     private InputStream asStream(String fullPath) {
         return getClass().getResourceAsStream(fullPath);
+    }
+    
+    private Year year(String id, String year, String from, String to) {
+        return new Year(new YearId(id), year, LocalDate.parse(from), LocalDate.parse(to));
+    }
+    
+    private <T> Set<T> set(T... arr) {
+        return set(Arrays.asList(arr));
+    }
+    
+    private <T> Set<T> set(List<T> l) {
+        return new HashSet<>(l);
     }
 }
