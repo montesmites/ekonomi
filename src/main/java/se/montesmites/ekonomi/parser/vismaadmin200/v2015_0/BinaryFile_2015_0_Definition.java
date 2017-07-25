03850b54e3fd549ff0ba3d350d76fa8071aa10a2
nrknik <nkrnik@montesmites.se>
2017-07-25 14:31:31 +0200

    BinaryFile_2015_0_Definition changed to class from enum

--- src/main/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/BinaryFile_2015_0_Definition.java
+++ src/main/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/BinaryFile_2015_0_Definition.java
@@ -1,17 +1,20 @@
 package se.montesmites.ekonomi.parser.vismaadmin200.v2015_0;
 
 import java.util.List;
+import se.montesmites.ekonomi.model.Year;
 import static se.montesmites.ekonomi.parser.vismaadmin200.DataType.*;
 import se.montesmites.ekonomi.parser.vismaadmin200.Field;
 import static se.montesmites.ekonomi.parser.vismaadmin200.v2015_0.BinaryFile_2015_0_DefinitionUtil.*;
 
-enum BinaryFile_2015_0_Definition {
-    YEARS("BOKFAAR.DBF", 513, 89, fields(
-            field("status", STRING, 0, 1),
-            field("yearid", STRING, 1, 1),
-            field("year", STRING, 2, 4),
-            field("from", DATE, 2, 8),
-            field("to", DATE, 10, 8))
+class BinaryFile_2015_0_Definition<T> {
+
+    public final static BinaryFile_2015_0_Definition YEARS = new BinaryFile_2015_0_Definition<Year>(
+            "BOKFAAR.DBF", 513, 89, fields(
+                    field("status", STRING, 0, 1),
+                    field("yearid", STRING, 1, 1),
+                    field("year", STRING, 2, 4),
+                    field("from", DATE, 2, 8),
+                    field("to", DATE, 10, 8))
     );
 
     private final String fileName;
@@ -37,7 +40,7 @@ enum BinaryFile_2015_0_Definition {
     public int getLength() {
         return length;
     }
-    
+
     public List<Field<?>> getFields() {
         return fields;
     }
