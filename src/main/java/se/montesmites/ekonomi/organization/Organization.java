ffba4ea88e300508c4d905486fa3fc3fc128e9cd
nrknik <nkrnik@montesmites.se>
2017-07-28 20:47:25 +0200

    get Entries by EventId from Organization

--- src/main/java/se/montesmites/ekonomi/organization/Organization.java
+++ src/main/java/se/montesmites/ekonomi/organization/Organization.java
@@ -2,6 +2,7 @@ package se.montesmites.ekonomi.organization;
 
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import se.montesmites.ekonomi.model.Event;
@@ -9,7 +10,8 @@ import se.montesmites.ekonomi.model.EventId;
 import se.montesmites.ekonomi.parser.vismaadmin200.Parser;
 import static se.montesmites.ekonomi.parser.vismaadmin200.v2015_0.BinaryFile_2015_0.*;
 import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
+import se.montesmites.ekonomi.model.Entry;
 import se.montesmites.ekonomi.model.Year;
 
 public class Organization {
@@ -17,20 +19,25 @@ public class Organization {
     public static Organization fromPath(Path path) {
         Parser p = new Parser(path);
         return new Organization(
+                p.parse(ENTRIES),
                 p.parse(EVENTS),
                 p.parse(YEARS));
     }
 
     private final Map<java.time.Year, Year> yearsByYear;
     private final Map<EventId, Event> eventsByEventId;
+    private final Map<EventId, List<Entry>> entriesByEventId;
 
     private Organization(
+            Collection<Entry> entries,
             Collection<Event> events,
             Collection<Year> years) {
         this.yearsByYear = years.stream()
                 .collect(toMap(Year::getYear, identity()));
         this.eventsByEventId = events.stream()
                 .collect(toMap(Event::getEventId, identity()));
+        this.entriesByEventId = entries.stream()
+                .collect(groupingBy(Entry::getEventId));
     }
 
     public Optional<Year> getYear(java.time.Year year) {
@@ -39,5 +46,9 @@ public class Organization {
 
     public Optional<Event> getEvent(EventId eventId) {
         return Optional.ofNullable(eventsByEventId.get(eventId));
+    }
+
+    public Optional<List<Entry>> getEntries(EventId eventId) {
+        return Optional.ofNullable(entriesByEventId.get(eventId));
     }
 }
--- src/test/java/se/montesmites/ekonomi/organization/OrganizationTest.java
+++ src/test/java/se/montesmites/ekonomi/organization/OrganizationTest.java
@@ -1,36 +1,46 @@
 package se.montesmites.ekonomi.organization;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import se.montesmites.ekonomi.model.Entry;
 import se.montesmites.ekonomi.model.Event;
 import se.montesmites.ekonomi.model.EventId;
 import se.montesmites.ekonomi.model.Series;
 import se.montesmites.ekonomi.model.Year;
 import se.montesmites.ekonomi.model.YearId;
 import se.montesmites.ekonomi.test.util.ResourceToFileCopier;
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+import se.montesmites.ekonomi.model.AccountId;
+import se.montesmites.ekonomi.model.Currency;
+import se.montesmites.ekonomi.model.EntryEvent;
+import se.montesmites.ekonomi.model.EntryStatus;
 
 public class OrganizationTest {
+
     @ClassRule
     public static TemporaryFolder tempfolder = new TemporaryFolder();
 
     private Organization organization;
-    
+
     @BeforeClass
     public static void beforeClass() throws Exception {
         ResourceToFileCopier copier = new ResourceToFileCopier();
         copier.copyAll(tempfolder);
     }
-    
+
     @Before
     public void before() throws Exception {
         this.organization = Organization.fromPath(tempfolder.getRoot().toPath());
     }
-    
+
     @Test
     public void readYear_byYear_2012() throws Exception {
         Year year = organization.getYear(java.time.Year.of(2012)).get();
@@ -49,5 +59,24 @@ public class OrganizationTest {
         assertEquals(LocalDate.parse("2012-01-12"), event.getDate());
         assertEquals("Överföring till sparkonto", event.getDescription());
         assertEquals(LocalDate.parse("2011-01-31"), event.getRegistrationDate());
+    }
+
+    @Test
+    public void readEntries_byEventId_2012A1() throws Exception {
+        YearId yearId = organization.getYear(java.time.Year.of(2012)).get().getYearId();
+        EventId eventId = new EventId(yearId, 1, new Series("A"));
+        List<Entry> actEntries = organization.getEntries(eventId).get().stream()
+                .sorted(comparing(entry -> entry.getAccountId().getId()))
+                .collect(toList());
+        List<Entry> expEntries = Arrays.asList(
+                entry(eventId, "1920", -50000000),
+                entry(eventId, "1940", 50000000));
+        assertEquals(expEntries, actEntries);
+    }
+
+    private Entry entry(EventId eventId, String account, long amount) {
+        return new Entry(eventId, new AccountId(eventId.getYearId(), account),
+                new Currency(amount), new EntryStatus(EntryStatus.Status.ACTIVE,
+                        EntryEvent.ORIGINAL));
     }
 }
