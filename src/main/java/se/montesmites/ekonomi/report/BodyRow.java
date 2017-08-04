260238de2f741cac2b83b48cc6cd436fc925dac2
nrknik <nkrnik@montesmites.se>
2017-08-04 18:01:51 +0200

    BodyRow => prepare for more than one account

--- src/main/java/se/montesmites/ekonomi/report/BodyRow.java
+++ src/main/java/se/montesmites/ekonomi/report/BodyRow.java
@@ -1,7 +1,9 @@
 package se.montesmites.ekonomi.report;
 
+import java.time.Month;
 import java.time.YearMonth;
 import java.util.Optional;
+import java.util.stream.Stream;
 import se.montesmites.ekonomi.model.AccountId;
 import se.montesmites.ekonomi.model.Currency;
 
@@ -29,18 +31,27 @@ public class BodyRow implements Row {
             case TOTAL:
                 return getYearlyTotal().format();
             default:
-                return getMonthlyAmount(column).orElse(new Currency(0)).format();
+                return Stream.of(accountId)
+                        .map(acc -> getMonthlyAmount(acc, column.getMonth()))
+                        .map(amount -> amount.orElse(new Currency(0)))
+                        .reduce(new Currency(0), (sum, term) -> sum.add(term))
+                        .format();
         }
     }
 
     public Optional<Currency> getMonthlyAmount(Column column) {
-        return column.getMonth()
-                .flatMap(month
-                        -> fetcher.fetchAmount(
+        return getMonthlyAmount(accountId, column.getMonth());
+    }
+
+    private Optional<Currency> getMonthlyAmount(AccountId accountId, Optional<Month> month) {
+        return month.flatMap(m
+                -> getMonthlyAmount(
                         accountId,
-                        YearMonth.of(
-                                year.getValue(),
-                                month)));
+                        YearMonth.of(year.getValue(), m)));
+    }
+
+    private Optional<Currency> getMonthlyAmount(AccountId accountId, YearMonth yearMonth) {
+        return fetcher.fetchAmount(accountId, yearMonth);
     }
 
     public Currency getYearlyTotal() {
