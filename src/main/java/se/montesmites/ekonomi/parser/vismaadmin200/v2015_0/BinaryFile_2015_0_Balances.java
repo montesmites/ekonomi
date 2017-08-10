a376eb5036272d5f96020083c3515a1741e0aea5
nrknik <nkrnik@montesmites.se>
2017-08-10 17:39:12 +0200

    BinaryFile_2015_0_Balances => read possible asterisk before balance

--- src/main/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/BinaryFile_2015_0_Balances.java
+++ src/main/java/se/montesmites/ekonomi/parser/vismaadmin200/v2015_0/BinaryFile_2015_0_Balances.java
@@ -14,15 +14,22 @@ import se.montesmites.ekonomi.parser.vismaadmin200.Record;
 public class BinaryFile_2015_0_Balances extends BinaryFile_2015_0<Balance> {
 
     public BinaryFile_2015_0_Balances() {
-        super("REING.DBF", 322, 78);
+        super("REING.DBF", 321, 78);
     }
-    private final Field<String> YEARID = new Field("yearid", STRING, 0, 1);
-    private final Field<String> ACCOUNT = new Field("account", STRING, 9, 4);
-    private final Field<Currency> AMOUNT = new Field("mnt", CURRENCY, 14, 14);
+
+    private final Field<String> YEARID = new Field("yearid", STRING, 1, 1);
+    private final Field<String> ACCOUNT = new Field("account", STRING, 10, 4);
+    private final Field<Currency> AMOUNT = new Field("mnt", CURRENCY, 15, 14);
+    private final Field<String> ASTERISK = new Field("asterisk", STRING, 0, 1) {
+        @Override
+        public boolean filter(Record record) {
+            return super.filter(record) && !"*".equals(ASTERISK.extract(record));
+        }
+    };
 
     @Override
     List<Field<?>> getFields() {
-        return Arrays.asList(YEARID, ACCOUNT, AMOUNT);
+        return Arrays.asList(ASTERISK, YEARID, ACCOUNT, AMOUNT);
     }
 
     @Override
