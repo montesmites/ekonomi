b210ae99d30c48b19af1127df3faec0984cf04a2
nrknik <nkrnik@montesmites.se>
2017-08-10 08:20:42 +0200

    CashflowReport_OneSection_OneRow_Test => seriously refactored

---
+++ src/test/java/se/montesmites/ekonomi/report/CashflowReport_AccountGroup_2012.java
@@ -0,0 +1,136 @@
+package se.montesmites.ekonomi.report;
+
+import java.util.AbstractMap;
+import static java.util.Comparator.comparing;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import java.util.stream.Stream;
+import static org.junit.Assert.assertEquals;
+import se.montesmites.ekonomi.model.AccountId;
+import se.montesmites.ekonomi.model.Currency;
+import static se.montesmites.ekonomi.report.Column.APRIL;
+import static se.montesmites.ekonomi.report.Column.AUGUST;
+import static se.montesmites.ekonomi.report.Column.DECEMBER;
+import static se.montesmites.ekonomi.report.Column.DESCRIPTION;
+import static se.montesmites.ekonomi.report.Column.FEBRUARY;
+import static se.montesmites.ekonomi.report.Column.JANUARY;
+import static se.montesmites.ekonomi.report.Column.JULY;
+import static se.montesmites.ekonomi.report.Column.JUNE;
+import static se.montesmites.ekonomi.report.Column.MARCH;
+import static se.montesmites.ekonomi.report.Column.MAY;
+import static se.montesmites.ekonomi.report.Column.NOVEMBER;
+import static se.montesmites.ekonomi.report.Column.OCTOBER;
+import static se.montesmites.ekonomi.report.Column.SEPTEMBER;
+
+;
+
+public enum CashflowReport_AccountGroup_2012 {
+    BOKFORT_RESULTAT(
+            "Bokfört resultat",
+            "([3-7]\\d|8[1-8])\\d\\d",
+            new EnumMap<Column, Currency>(Column.class) {
+        {
+            put(JANUARY, new Currency(2866947));
+            put(FEBRUARY, new Currency(-6758901));
+            put(MARCH, new Currency(-14611584));
+            put(APRIL, new Currency(-2150417));
+            put(MAY, new Currency(6833003));
+            put(JUNE, new Currency(-30431649));
+            put(JULY, new Currency(4877975));
+            put(AUGUST, new Currency(-3810720));
+            put(SEPTEMBER, new Currency(-4316052));
+            put(OCTOBER, new Currency(123642));
+            put(NOVEMBER, new Currency(-21238571));
+            put(DECEMBER, new Currency(21533255));
+        }
+    }
+    );
+
+    private final static java.time.Year YEAR = java.time.Year.of(2012);
+
+    public static Stream<BodyRow> bodyRowsOf(
+            CashflowDataFetcher fetcher,
+            List<CashflowReport_AccountGroup_2012> groups) {
+        return groups.stream().map(group -> group.bodyRow(fetcher));
+    }
+
+    public static void assertBodyRowDescriptions(
+            Section section,
+            List<CashflowReport_AccountGroup_2012> groups) {
+        final List<String> exp
+                = groups.stream().map(g -> g.description).collect(toList());
+        final List<String> act
+                = section.streamBodyRows()
+                        .map(row -> row.getText(DESCRIPTION))
+                        .collect(toList());
+        assertEquals(exp.size(), act.size());
+        for (int i = 0; i < exp.size(); i++) {
+            String fmt = "index %d";
+            String msg = String.format(fmt, i);
+            assertEquals(msg, exp.get(i), act.get(i));
+        }
+    }
+
+    public static void assertMonthlyAmounts(
+            Section section,
+            List<CashflowReport_AccountGroup_2012> groups) {
+        final List<Map<Column, Currency>> expList
+                = groups.stream()
+                        .map(group -> group.expectedAmounts)
+                        .collect(toList());
+        final List<Map<Column, Currency>> actList
+                = section.streamBodyRows()
+                        .map(row -> Column.streamMonths()
+                        .map(col -> new AbstractMap.SimpleEntry<>(col,
+                        row.getMonthlyAmount(col)))
+                        .collect(
+                                toMap(
+                                        Map.Entry::getKey,
+                                        Map.Entry::getValue)))
+                        .collect(toList());
+        assertEquals(expList.size(), actList.size());
+        for (int i = 0; i < expList.size(); i++) {
+            final int ix = i;
+            final Map<Column, Currency> exp = expList.get(i);
+            final Map<Column, Currency> act = actList.get(i);
+            final String fmt = "%s at %s: ";
+            Column.streamMonths().forEach(column
+                    -> assertEquals(
+                            String.format(fmt, column.name(), ix),
+                            exp.get(column),
+                            act.get(column))
+            );
+        }
+    }
+
+    private final String description;
+    private final String regex;
+    private final Map<Column, Currency> expectedAmounts;
+
+    private CashflowReport_AccountGroup_2012(
+            String description,
+            String regex,
+            Map<Column, Currency> expectedAmounts) {
+        this.description = description;
+        this.regex = regex;
+        this.expectedAmounts = expectedAmounts;
+    }
+
+    private BodyRow bodyRow(CashflowDataFetcher fetcher) {
+        final AccountFilter filter = new AccountFilterByRegex(regex);
+        List<AccountId> accountIds
+                = filter
+                        .filter(fetcher.streamAccountIds(YEAR))
+                        .distinct()
+                        .sorted(comparing(AccountId::getId))
+                        .collect(toList());
+        return new DefaultBodyRow(
+                fetcher,
+                () -> accountIds.stream(),
+                YEAR,
+                description);
+    }
+}
--- src/test/java/se/montesmites/ekonomi/report/CashflowReport_OneSection_OneRow_Test.java
+++ src/test/java/se/montesmites/ekonomi/report/CashflowReport_OneSection_OneRow_Test.java
@@ -1,15 +1,8 @@
 package se.montesmites.ekonomi.report;
 
 import java.time.Year;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.EnumMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
@@ -17,9 +10,8 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import se.montesmites.ekonomi.model.AccountId;
-import se.montesmites.ekonomi.model.Currency;
 import se.montesmites.ekonomi.organization.Organization;
+import static se.montesmites.ekonomi.report.CashflowReport_AccountGroup_2012.*;
 import static se.montesmites.ekonomi.report.Column.*;
 import se.montesmites.ekonomi.test.util.ResourceToFileCopier;
 
@@ -29,7 +21,6 @@ public class CashflowReport_OneSection_OneRow_Test {
     public static TemporaryFolder tempfolder = new TemporaryFolder();
 
     private final static String DEN_LOPANDE_VERKSAMHETEN = "Den löpande verksamheten";
-    private final static String BOKFORT_RESULTAT = "Bokfört resultat";
 
     private final Year year = Year.of(2012);
 
@@ -37,6 +28,7 @@ public class CashflowReport_OneSection_OneRow_Test {
     private CashflowDataFetcher fetcher;
     private CashflowReport report;
     private Section section;
+    private List<CashflowReport_AccountGroup_2012> groups;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -49,33 +41,16 @@ public class CashflowReport_OneSection_OneRow_Test {
         this.organization = Organization.fromPath(tempfolder.getRoot().toPath());
         this.fetcher = new CashflowDataFetcher(this.organization);
         this.report = new CashflowReport(fetcher, year, () -> sections());
-        this.section = new Section(DEN_LOPANDE_VERKSAMHETEN, fetcher, year, () -> bodyRows());
+        this.groups = Arrays.asList(BOKFORT_RESULTAT);
+        this.section = new Section(
+                DEN_LOPANDE_VERKSAMHETEN,
+                fetcher,
+                year,
+                () -> bodyRowsOf(fetcher, groups));
     }
 
     private Stream<Section> sections() {
         return Stream.of(section);
-    }
-
-    private Stream<BodyRow> bodyRows() {
-        return Stream.of(
-                bodyRow(
-                        filterAccounts("([3-7]\\d|8[1-8])\\d\\d"),
-                        BOKFORT_RESULTAT));
-    }
-
-    private Supplier<Stream<AccountId>> filterAccounts(String regex) {
-        final AccountFilter filter = new AccountFilterByRegex(regex);
-        final Set<AccountId> accounts = filter.filter(
-                fetcher.streamAccountIds(year)).collect(toSet());
-        return () -> accounts.stream();
-    }
-
-    private BodyRow bodyRow(Supplier<Stream<AccountId>> accountIds, String description) {
-        return new DefaultBodyRow(
-                fetcher,
-                accountIds,
-                year,
-                description);
     }
 
     @Test
@@ -92,46 +67,11 @@ public class CashflowReport_OneSection_OneRow_Test {
 
     @Test
     public void body_rowDescription() {
-        final String exp = BOKFORT_RESULTAT;
-        final List<String> act
-                = section.streamBodyRows()
-                        .map(row -> row.getText(DESCRIPTION))
-                        .collect(toList());
-        assertEquals(1, act.size());
-        assertEquals(exp, act.get(0));
+        assertBodyRowDescriptions(section, groups);
     }
 
     @Test
     public void testMonthlyAmountsForGroupedRow() {
-        final Map<Column, Currency> exp = new EnumMap<Column, Currency>(
-                Column.class) {
-            {
-                put(JANUARY, new Currency(2866947));
-                put(FEBRUARY, new Currency(-6758901));
-                put(MARCH, new Currency(-14611584));
-                put(APRIL, new Currency(-2150417));
-                put(MAY, new Currency(6833003));
-                put(JUNE, new Currency(-30431649));
-                put(JULY, new Currency(4877975));
-                put(AUGUST, new Currency(-3810720));
-                put(SEPTEMBER, new Currency(-4316052));
-                put(OCTOBER, new Currency(123642));
-                put(NOVEMBER, new Currency(-21238571));
-                put(DECEMBER, new Currency(21533255));
-            }
-        };
-        final List<Map<Column, Currency>> actList = section.streamBodyRows()
-                .map(row -> Column.streamMonths()
-                .map(col -> new SimpleEntry<>(col, row.getMonthlyAmount(col)))
-                .collect(
-                        toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue)))
-                .collect(toList());
-        final Map<Column, Currency> act = actList.get(0);
-        assertEquals(1, actList.size());
-        Column.streamMonths().forEach(column
-                -> assertEquals(column.name(), exp.get(column), act.get(column))
-        );
+        assertMonthlyAmounts(section, groups);
     }
 }
