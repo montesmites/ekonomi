525e2f89786879d604cf1fd9e601f585dfd75cfc
nrknik <nkrnik@montesmites.se>
2017-08-12 07:44:08 +0200

    Organization => get Year by YearId

--- src/main/java/se/montesmites/ekonomi/organization/Organization.java
+++ src/main/java/se/montesmites/ekonomi/organization/Organization.java
@@ -15,6 +15,7 @@ import se.montesmites.ekonomi.model.Entry;
 import se.montesmites.ekonomi.model.Event;
 import se.montesmites.ekonomi.model.EventId;
 import se.montesmites.ekonomi.model.Year;
+import se.montesmites.ekonomi.model.YearId;
 import se.montesmites.ekonomi.parser.vismaadmin200.Parser;
 import static se.montesmites.ekonomi.parser.vismaadmin200.v2015_0.BinaryFile_2015_0.*;
 
@@ -39,6 +40,7 @@ public class Organization {
     private final Map<EventId, List<Entry>> entriesByEventId;
     private final Map<EventId, Event> eventsByEventId;
     private final Map<java.time.Year, Year> yearsByYear;
+    private final Map<YearId, Year> yearsByYearId;
 
     private Organization(
             Collection<Account> accounts,
@@ -60,6 +62,8 @@ public class Organization {
                 .collect(toMap(Event::getEventId, identity()));
         this.yearsByYear = years.stream()
                 .collect(toMap(Year::getYear, identity()));
+        this.yearsByYearId = years.stream()
+                .collect(toMap(Year::getYearId, identity()));
     }
     
     public Stream<Account> streamAccounts() {
@@ -76,6 +80,10 @@ public class Organization {
     
     public Optional<Year> getYear(java.time.Year year) {
         return Optional.ofNullable(yearsByYear.get(year));
+    }
+    
+    public Optional<Year> getYear(YearId yearId) {
+        return Optional.ofNullable(yearsByYearId.get(yearId));
     }
 
     public Optional<Event> getEvent(EventId eventId) {
