44b924314dbcbbf6cdbe03f0ab150c7de161ce66
nrknik <nkrnik@montesmites.se>
2017-08-15 08:54:54 +0200

    Row => default Optional<RowWithAmounts> asRowWithAmounts()

--- src/main/java/se/montesmites/ekonomi/report/Row.java
+++ src/main/java/se/montesmites/ekonomi/report/Row.java
@@ -1,5 +1,11 @@
 package se.montesmites.ekonomi.report;
 
+import java.util.Optional;
+
 public interface Row {
     public String getText(Column column);
+    
+    default Optional<RowWithAmounts> asRowWithAmounts() {
+        return Optional.empty();
+    }
 }
