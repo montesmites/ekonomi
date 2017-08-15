04de8888716a3fb6a7f004f97740c17fca72a5ae
nrknik <nkrnik@montesmites.se>
2017-08-15 09:11:31 +0200

    BodyRow => implemented Optional<RowWithAmounts> asRowWithAmounts()

--- src/main/java/se/montesmites/ekonomi/report/BodyRow.java
+++ src/main/java/se/montesmites/ekonomi/report/BodyRow.java
@@ -1,5 +1,6 @@
 package se.montesmites.ekonomi.report;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 import se.montesmites.ekonomi.model.AccountId;
@@ -7,4 +8,9 @@ import se.montesmites.ekonomi.model.AccountId;
 public interface BodyRow extends Row, RowWithAmounts {
 
     public Supplier<Stream<AccountId>> getAccountIds();
+
+    @Override
+    default Optional<RowWithAmounts> asRowWithAmounts() {
+        return Optional.of(this);
+    }
 }
