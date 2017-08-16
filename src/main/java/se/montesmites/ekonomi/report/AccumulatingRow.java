ee8c9a1441238f1c4eabd3f19fe8e5cd97129f24
nrknik <nkrnik@montesmites.se>
2017-08-16 06:24:39 +0200

    AccumulatingRow => implemented

---
+++ src/main/java/se/montesmites/ekonomi/report/AccumulatingRow.java
@@ -0,0 +1,87 @@
+package se.montesmites.ekonomi.report;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import se.montesmites.ekonomi.model.AccountId;
+import se.montesmites.ekonomi.model.Balance;
+import se.montesmites.ekonomi.model.Currency;
+import static se.montesmites.ekonomi.report.Column.*;
+
+public class AccumulatingRow implements RowWithAccounts {
+
+    private final CashflowDataFetcher fetcher;
+    private final Supplier<Stream<AccountId>> accountIds;
+    private final java.time.Year year;
+    private final DefaultRowWithAccounts monthlyNetAmounts;
+    private final Map<Column, Currency> amounts;
+
+    public AccumulatingRow(CashflowDataFetcher fetcher, Supplier<Stream<AccountId>> accountIds, java.time.Year year) {
+        this.fetcher = fetcher;
+        this.accountIds = accountIds;
+        this.year = year;
+        this.monthlyNetAmounts
+                = new DefaultRowWithAccounts(fetcher, accountIds, year, "");
+        this.amounts = getAmounts();
+    }
+
+    @Override
+    public Supplier<Stream<AccountId>> getAccountIds() {
+        return accountIds;
+    }
+
+    @Override
+    public String getText(Column column) {
+        return amounts.get(column).format();
+    }
+
+    @Override
+    public Currency getMonthlyAmount(Column column) {
+        return amounts.get(column);
+    }
+
+    @Override
+    public Currency getYearlyTotal() {
+        return amounts.get(DECEMBER);
+    }
+    
+    public Currency getBalance() {
+        return amounts.get(DESCRIPTION);
+    }
+    
+    private Map<Column, Currency> getAmounts() {
+        Map<Column, Currency> map = new EnumMap<>(Column.class);
+        Currency accumulator = new Currency(0);
+        for (Column column : Column.values()) {
+            Currency net = columnNetAmount(column);
+            Currency columnBalance = accumulator.add(net);
+            map.put(column, columnBalance);
+            accumulator = columnBalance;
+        }
+        return map;
+    }
+
+    private Currency columnNetAmount(Column column) {
+        switch (column) {
+            case DESCRIPTION: return balance();
+            case TOTAL: return new Currency(0);
+            default: return monthlyNetAmounts.getMonthlyAmount(column);
+        }
+    }
+    
+    private Currency balance() {
+        return accountIds.get()
+                .map(this::balance)
+                .reduce(new Currency(0), Currency::add);
+    }
+
+    private Currency balance(AccountId accountId) {
+        return fetcher.fetchBalance(accountId)
+                .map(Balance::getBalance)
+                .map(Currency::getAmount)
+                .map(amount -> amount * fetcher.getCoefficient(accountId))
+                .map(Currency::new)
+                .orElse(new Currency(0));
+    }
+}
---
+++ src/test/java/se/montesmites/ekonomi/report/AccumulatingRowTest.java
@@ -0,0 +1,82 @@
+package se.montesmites.ekonomi.report;
+
+import java.time.Year;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import se.montesmites.ekonomi.model.AccountId;
+import se.montesmites.ekonomi.model.Currency;
+import se.montesmites.ekonomi.model.YearId;
+import se.montesmites.ekonomi.organization.Organization;
+import static se.montesmites.ekonomi.report.Column.*;
+import se.montesmites.ekonomi.test.util.ResourceToFileCopier;
+
+public class AccumulatingRowTest {
+
+    @ClassRule
+    public static TemporaryFolder tempfolder = new TemporaryFolder();
+
+    private final Year year = Year.of(2012);
+
+    private YearId yearId;
+    private Organization organization;
+    private CashflowDataFetcher fetcher;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        ResourceToFileCopier copier = new ResourceToFileCopier();
+        copier.copyAll(tempfolder);
+    }
+
+    @Before
+    public void before() throws Exception {
+        this.organization = Organization.fromPath(tempfolder.getRoot().toPath());
+        this.fetcher = new CashflowDataFetcher(this.organization, __ -> 1);
+        this.yearId = organization.getYear(year).get().getYearId();
+    }
+
+    @Test
+    public void fourAccounts() {
+        AccumulatingRow row = new AccumulatingRow(
+                fetcher,
+                () -> Stream.of("1910", "1920", "1930", "1940")
+                        .map(account -> new AccountId(yearId, account)),
+                year
+        );
+        assertEquals(expectedAmounts().get(DESCRIPTION), row.getBalance());
+        Column.streamMonths().forEach(month
+                -> assertEquals(
+                        month.name(),
+                        expectedAmounts().get(month),
+                        row.getMonthlyAmount(month)));
+        assertEquals(
+                expectedAmounts().get(DECEMBER),
+                row.getYearlyTotal());
+    }
+
+    private Map<Column, Currency> expectedAmounts() {
+        return new EnumMap< Column, Currency>(Column.class) {
+            {
+                put(DESCRIPTION, new Currency(127622521));
+                put(JANUARY, new Currency(140745321));
+                put(FEBRUARY, new Currency(140888401));
+                put(MARCH, new Currency(185524301));
+                put(APRIL, new Currency(117604301));
+                put(MAY, new Currency(127820601));
+                put(JUNE, new Currency(159100301));
+                put(JULY, new Currency(196575843));
+                put(AUGUST, new Currency(184371543));
+                put(SEPTEMBER, new Currency(161756373));
+                put(OCTOBER, new Currency(190541831));
+                put(NOVEMBER, new Currency(207509032));
+                put(DECEMBER, new Currency(241088832));
+            }
+        };
+    }
+}
