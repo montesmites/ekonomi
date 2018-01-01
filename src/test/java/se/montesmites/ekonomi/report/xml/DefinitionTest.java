a5677daab2744fcd33d5a1e59b51cc5e7c6c058b
nrknik <nkrnik@montesmites.se>
2018-01-01 08:45:23 +0100

    xml definition -> more tests

--- src/test/java/se/montesmites/ekonomi/report/xml/DefinitionTest.java
+++ src/test/java/se/montesmites/ekonomi/report/xml/DefinitionTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import se.montesmites.ekonomi.organization.Organization;
 import se.montesmites.ekonomi.report.AccountFilterByRegex;
 import se.montesmites.ekonomi.report.ReportBuilder;
+import se.montesmites.ekonomi.report.RowBuilder;
+import se.montesmites.ekonomi.report.SectionBuilder;
 import testdata.DefaultTestDataExtension;
 import testdata.OrganizationInjector;
 
 import javax.xml.bind.JAXB;
 import java.time.Year;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,18 +24,57 @@ class DefinitionTest {
     @OrganizationInjector
     private Organization organization;
 
+    private XmlDefinition xmlDefinition;
+    private ReportBuilder reportBuilder;
+
     @Test
     void t01_oneSectionOneRowSimpleDefinition() {
         final String path = "/se/montesmites/ekonomi/report/xml/01_one-section-one-row-simple-definition.xml";
-        final XmlDefinition definition = JAXB.unmarshal(getClass().getResourceAsStream(path), XmlDefinition.class);
-        final ReportBuilder reportBuilder = definition.toReportBuilder(organization, year);
+        this.xmlDefinition = JAXB.unmarshal(getClass().getResourceAsStream(path), XmlDefinition.class);
+        this.reportBuilder = xmlDefinition.toReportBuilder(organization, year);
         assertAll("report",
-                  () -> assertEquals(1, reportBuilder.getSectionBuilders().size()),
-                  () -> assertEquals("Section 1", reportBuilder.getSectionBuilders().get(0).getDescription()),
-                  () -> assertEquals(1, reportBuilder.getSectionBuilders().get(0).getBodyRowBuilders().size()),
-                  () -> assertEquals("Account Group 1", reportBuilder.getSectionBuilders().get(0).getBodyRowBuilders().get(0).getDescription()),
-                  () -> assertEquals(AccountFilterByRegex.class, reportBuilder.getSectionBuilders().get(0).getBodyRowBuilders().get(0).getFilter().getClass()),
-                  () -> assertEquals("Account Group 1", ((AccountFilterByRegex) reportBuilder.getSectionBuilders().get(0).getBodyRowBuilders().get(0).getFilter()).getPattern().pattern())
+                  () -> assertEquals(1, getSectionBuilders().size()),
+                  () -> assertEquals("Section 1", getSectionBuilderAt(0).getDescription()),
+                  () -> assertEquals(1, getBodyRowBuildersAt(0).size()),
+                  () -> assertEquals("Account Group 1", getBodyRowBuilderAt(0, 0).getDescription()),
+                  () -> assertEquals("Account Group 1", getRegexPatternAt(0,0))
         );
+    }
+
+    @Test
+    void t02_oneSectionTwoRowsSimpleDefinition() {
+        final String path = "/se/montesmites/ekonomi/report/xml/02_one-section-two-rows-simple-definition.xml";
+        this.xmlDefinition = JAXB.unmarshal(getClass().getResourceAsStream(path), XmlDefinition.class);
+        this.reportBuilder = xmlDefinition.toReportBuilder(organization, year);
+        assertAll("report",
+                  () -> assertEquals(1, getSectionBuilders().size()),
+                  () -> assertEquals("Section 1", getSectionBuilderAt(0).getDescription()),
+                  () -> assertEquals(2, getBodyRowBuildersAt(0).size()),
+                  () -> assertEquals("Account Group 1", getBodyRowBuilderAt(0, 0).getDescription()),
+                  () -> assertEquals("Account Group 1", getRegexPatternAt(0, 0)),
+                  () -> assertEquals("Account Group 2", getBodyRowBuilderAt(0, 1).getDescription()),
+                  () -> assertEquals("Account Group 2", getRegexPatternAt(0, 1))
+        );
+    }
+
+    private List<SectionBuilder> getSectionBuilders() {
+        return reportBuilder.getSectionBuilders();
+    }
+
+    private SectionBuilder getSectionBuilderAt(int section) {
+        return getSectionBuilders().get(section);
+    }
+
+    private List<RowBuilder> getBodyRowBuildersAt(int section) {
+        return getSectionBuilderAt(section).getBodyRowBuilders();
+    }
+
+    private RowBuilder getBodyRowBuilderAt(int section, int row) {
+        return getBodyRowBuildersAt(section).get(row);
+    }
+
+    private String getRegexPatternAt(int section, int row) {
+        AccountFilterByRegex filter = (AccountFilterByRegex) getBodyRowBuilderAt(section, row).getFilter();
+        return filter.getPattern().pattern();
     }
 }
