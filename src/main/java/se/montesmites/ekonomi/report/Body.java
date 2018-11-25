6bca3fc0923ec014bc8f8e2f55003a7f943dd614
nrknik <nkrnik@montesmites.se>
2018-11-25 06:45:43 +0100

    introduced Body

---
+++ src/main/java/se/montesmites/ekonomi/report/Body.java
@@ -0,0 +1,25 @@
+package se.montesmites.ekonomi.report;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public interface Body {
+
+  static Body empty() {
+    return Stream::empty;
+  }
+
+  static Body of(Supplier<Stream<RowWithAmounts>> rowWithAmounts) {
+    return rowWithAmounts::get;
+  }
+
+  static Body of(RowWithAmounts rowWithAmounts) {
+    return () -> Stream.of(rowWithAmounts);
+  }
+
+  default Body add(RowWithAmounts rowWithAmounts) {
+    return () -> Stream.concat(this.stream(), Stream.of(rowWithAmounts));
+  }
+
+  Stream<RowWithAmounts> stream();
+}
---
+++ src/test/java/se/montesmites/ekonomi/report/BodyTest.java
@@ -0,0 +1,59 @@
+package se.montesmites.ekonomi.report;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import se.montesmites.ekonomi.model.Currency;
+
+class BodyTest {
+
+  @Test
+  void emptyBody() {
+    var body = Body.empty();
+    var exp = List.<Row>of();
+    var act = body.stream().collect(toList());
+    assertBodys(exp, act);
+  }
+
+  @Test
+  void oneRowWithAmounts() {
+    var row = RowWithAmounts.of(column -> Currency.of(column.ordinal()));
+    var body = Body.of(row);
+    var exp = List.of(row);
+    var act = body.stream().collect(toList());
+    assertBodys(exp, act);
+  }
+
+  @Test
+  void twoRowsWithAmounts() {
+    var row1 = RowWithAmounts.of(column -> Currency.of(column.ordinal()));
+    var row2 = RowWithAmounts.of(column -> Currency.of(column.ordinal() * 100));
+    var body = Body.of(row1).add(row2);
+    var exp = List.of(row1, row2);
+    var act = body.stream().collect(toList());
+    assertBodys(exp, act);
+  }
+
+  @Test
+  void streamOfRowsWithAmounts() {
+    var row1 = RowWithAmounts.of(column -> Currency.of(column.ordinal()));
+    var row2 = RowWithAmounts.of(column -> Currency.of(column.ordinal() * 100));
+    var body = Body.of(() -> Stream.of(row1, row2));
+    var exp = List.of(row1, row2);
+    var act = body.stream().collect(toList());
+    assertBodys(exp, act);
+  }
+
+  private void assertBodys(List<? extends Row> exp, List<? extends Row> act) {
+    assertAll(
+        () -> assertEquals(exp.size(), act.size()),
+        () ->
+            assertAll(
+                () -> range(0, exp.size()).forEach(i -> exp.get(i).isEquivalentTo(act.get(i)))));
+  }
+}
