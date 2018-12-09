64e61bd5c1792f74078428b203bf8da0ab25f2a3
nrknik <nkrnik@montesmites.se>
2018-12-09 16:36:21 +0100

    inline NikkaSections into main program

--- src/main/java/se/montesmites/ekonomi/nikka/Main.java
+++ src/main/java/se/montesmites/ekonomi/nikka/Main.java
@@ -1,17 +1,13 @@
 package se.montesmites.ekonomi.nikka;
 
-import static se.montesmites.ekonomi.nikka.NikkaSection.BOENDE;
 import static se.montesmites.ekonomi.nikka.NikkaSection.FORANDRING_LIKVIDA_MEDEL;
-import static se.montesmites.ekonomi.nikka.NikkaSection.FORNODENHETER;
-import static se.montesmites.ekonomi.nikka.NikkaSection.INKOMSTER;
-import static se.montesmites.ekonomi.nikka.NikkaSection.JAMFORELSESTORANDE_POSTER;
-import static se.montesmites.ekonomi.nikka.NikkaSection.OVRIGT;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Year;
+import java.util.List;
 import java.util.stream.Stream;
 import se.montesmites.ekonomi.organization.OrganizationBuilder;
 import se.montesmites.ekonomi.report.AccountGroup;
@@ -42,10 +38,38 @@ class Main {
 
   private CashflowReport generateCashflowReport(Year year) {
     var reportBuilder = new ReportBuilder(fetcher, year);
-    var inkomster = INKOMSTER.toSection(reportBuilder);
-    var boende = BOENDE.toSection(reportBuilder);
-    var fornodenheter = FORNODENHETER.toSection(reportBuilder);
-    var ovrigt = OVRIGT.toSection(reportBuilder);
+    var inkomster =
+        reportBuilder.buildSection(
+            "Inkomster",
+            List.of(
+                AccountGroup.of("Löner och arvoden", "(30|36)\\d\\d"),
+                AccountGroup.of("Nettoomsättning övrigt", "3([1-5]|[7-9])\\d\\d")));
+    var boende =
+        reportBuilder.buildSection(
+            "Boende",
+            List.of(
+                AccountGroup.of("Månadsavgift m.m.", "501\\d"),
+                AccountGroup.of("Amortering Fredsgatan 13", "2353"),
+                AccountGroup.of("Bolån ränta och liknande", "84[019]\\d"),
+                AccountGroup.of("Hemförsäkring", "5081"),
+                AccountGroup.of("El (förbrukning och nät)", "502\\d"),
+                AccountGroup.of("Mobil, tv, bredband", "51\\d\\d")));
+    var fornodenheter =
+        reportBuilder.buildSection(
+            "Förnödenheter",
+            List.of(
+                AccountGroup.of("Dagligvaror", "40\\d\\d"),
+                AccountGroup.of("Kläder och skor", "41\\d\\d"),
+                AccountGroup.of("Kropp och själ", "42\\d\\d"),
+                AccountGroup.of("Personförsäkringar", "43\\d\\d"),
+                AccountGroup.of("A-kassa, fack, bank, skatt", "4[4-7]\\d\\d"),
+                AccountGroup.of("Transporter", "56\\d\\d")));
+    var ovrigt =
+        reportBuilder.buildSection(
+            "Övrigt",
+            List.of(
+                AccountGroup.of("Boende diverse", "5060|5[458]\\d\\d"),
+                AccountGroup.of("Övrigt", "(4[89]|[67]\\d)\\d\\d")));
     var foreJamforelsestorandePoster =
         reportBuilder.buildSection(
             inkomster
@@ -55,7 +79,15 @@ class Main {
                 .concat(ovrigt.body())
                 .aggregate("Före jämförelsestörande poster".toUpperCase())
                 .asRow());
-    var jamforelsestorandePoster = JAMFORELSESTORANDE_POSTER.toSection(reportBuilder);
+    var jamforelsestorandePoster =
+        reportBuilder.buildSection(
+            "Jämförelsestörande poster",
+            List.of(
+                AccountGroup.of("Kortfristigt netto", "(1[5-8]|2[4-9])\\d\\d"),
+                AccountGroup.of("Långfristigt netto", "(8[56]\\d\\d)|(10\\d\\d|13[456]\\d)"),
+                AccountGroup.of("Finansiellt netto", "(83\\d\\d)|(84[2-8]\\d)"),
+                AccountGroup.of("Investering boende", "11\\d\\d"),
+                AccountGroup.of("Extraordinärt netto", "87\\d\\d")));
     var forandringLikvidaMedel =
         reportBuilder.buildSection(
             FORANDRING_LIKVIDA_MEDEL
--- src/main/java/se/montesmites/ekonomi/nikka/NikkaSection.java
+++ src/main/java/se/montesmites/ekonomi/nikka/NikkaSection.java
@@ -10,42 +10,6 @@ import se.montesmites.ekonomi.report.ReportBuilder;
 import se.montesmites.ekonomi.report.Section;
 
 enum NikkaSection {
-  INKOMSTER(
-      "Inkomster",
-      List.of(
-          AccountGroup.of("Löner och arvoden", "(30|36)\\d\\d"),
-          AccountGroup.of("Nettoomsättning övrigt", "3([1-5]|[7-9])\\d\\d"))),
-  BOENDE(
-      "Boende",
-      List.of(
-          AccountGroup.of("Månadsavgift m.m.", "501\\d"),
-          AccountGroup.of("Amortering Fredsgatan 13", "2353"),
-          AccountGroup.of("Bolån ränta och liknande", "84[019]\\d"),
-          AccountGroup.of("Hemförsäkring", "5081"),
-          AccountGroup.of("El (förbrukning och nät)", "502\\d"),
-          AccountGroup.of("Mobil, tv, bredband", "51\\d\\d"))),
-  FORNODENHETER(
-      "Förnödenheter",
-      List.of(
-          AccountGroup.of("Dagligvaror", "40\\d\\d"),
-          AccountGroup.of("Kläder och skor", "41\\d\\d"),
-          AccountGroup.of("Kropp och själ", "42\\d\\d"),
-          AccountGroup.of("Personförsäkringar", "43\\d\\d"),
-          AccountGroup.of("A-kassa, fack, bank, skatt", "4[4-7]\\d\\d"),
-          AccountGroup.of("Transporter", "56\\d\\d"))),
-  OVRIGT(
-      "Övrigt",
-      List.of(
-          AccountGroup.of("Boende diverse", "5060|5[458]\\d\\d"),
-          AccountGroup.of("Övrigt", "(4[89]|[67]\\d)\\d\\d"))),
-  JAMFORELSESTORANDE_POSTER(
-      "Jämförelsestörande poster",
-      List.of(
-          AccountGroup.of("Kortfristigt netto", "(1[5-8]|2[4-9])\\d\\d"),
-          AccountGroup.of("Långfristigt netto", "(8[56]\\d\\d)|(10\\d\\d|13[456]\\d)"),
-          AccountGroup.of("Finansiellt netto", "(83\\d\\d)|(84[2-8]\\d)"),
-          AccountGroup.of("Investering boende", "11\\d\\d"),
-          AccountGroup.of("Extraordinärt netto", "87\\d\\d"))),
   FORANDRING_LIKVIDA_MEDEL(
       "Förändring likvida medel", List.of(AccountGroup.of("Likvida medel", "1493|19\\d\\d"))) {
     @Override
