b70a9936306c8a7014b32ad4f496c3b38797521c
nrknik <nkrnik@montesmites.se>
2019-08-09 06:07:25 +0200

    refactor

--- src/main/java/se/montesmites/ekonomi/sie/Sie4FileReaderAggregator.java
+++ src/main/java/se/montesmites/ekonomi/sie/Sie4FileReaderAggregator.java
@@ -1,9 +1,38 @@
 package se.montesmites.ekonomi.sie;
 
+import static se.montesmites.ekonomi.sie.Sie4FileReaderAggregator.AggregationStrategy.CHILD_RECORD_STRATEGY;
+import static se.montesmites.ekonomi.sie.Sie4FileReaderAggregator.AggregationStrategy.PARENT_RECORD_STRATEGY;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 class Sie4FileReaderAggregator {
+
+  enum AggregationStrategy {
+    PARENT_RECORD_STRATEGY(
+        (aggregator, line) -> {
+          aggregator.addRecord();
+          aggregator.parent = line;
+          aggregator.children = new ArrayList<>();
+          setParentRecordStrategy(aggregator);
+        }),
+    CHILD_RECORD_STRATEGY((aggregator, line) -> aggregator.children.add(SieRecord.of(line)));
+
+    private final BiConsumer<Sie4FileReaderAggregator, SieFileLine> strategy;
+
+    AggregationStrategy(BiConsumer<Sie4FileReaderAggregator, SieFileLine> strategy) {
+      this.strategy = strategy;
+    }
+
+    final void accept(Sie4FileReaderAggregator aggregator, SieFileLine line) {
+      strategy.accept(aggregator, line);
+    }
+
+    private static void setParentRecordStrategy(Sie4FileReaderAggregator aggregator) {
+      aggregator.strategy = PARENT_RECORD_STRATEGY;
+    }
+  }
 
   static Sie4FileReaderAggregator empty() {
     return new Sie4FileReaderAggregator();
@@ -12,7 +41,7 @@ class Sie4FileReaderAggregator {
   private List<SieRecord> records = new ArrayList<>();
   private SieFileLine parent;
   private List<SieRecord> children;
-  private boolean isChild = false;
+  private AggregationStrategy strategy = PARENT_RECORD_STRATEGY;
 
   private Sie4FileReaderAggregator() {
     this.children = new ArrayList<>();
@@ -25,18 +54,11 @@ class Sie4FileReaderAggregator {
 
   Sie4FileReaderAggregator accept(SieFileLine line) {
     if (line.getType() == SieFileLineType.BEGIN_SUBRECORDS) {
-      this.isChild = true;
+      this.strategy = CHILD_RECORD_STRATEGY;
     } else if (line.getType() == SieFileLineType.END_SUBRECORDS) {
-      this.isChild = false;
+      this.strategy = PARENT_RECORD_STRATEGY;
     } else if (line.getType() == SieFileLineType.PROSAIC) {
-      if (isChild) {
-        children.add(SieRecord.of(line));
-      } else {
-        addRecord();
-        this.parent = line;
-        this.children = new ArrayList<>();
-        this.isChild = false;
-      }
+      this.strategy.accept(this, line);
     } else {
       throw new IllegalStateException();
     }
