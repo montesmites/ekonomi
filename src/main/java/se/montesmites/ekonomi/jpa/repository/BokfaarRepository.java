1112c192c3d15155bf8e3b29b8bb535bc54bd6fc
nrknik <nrknik@montesmites.se>
2023-10-06 15:30:27 +0200

    remove legacy database tables

--- src/main/java/se/montesmites/ekonomi/jpa/repository/BokfaarRepository.java
+++
@@ -1,8 +0,0 @@
-package se.montesmites.ekonomi.jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se.montesmites.ekonomi.jpa.model.Bokfaar;
-
-@Repository
-public interface BokfaarRepository extends JpaRepository<Bokfaar, String> {}
